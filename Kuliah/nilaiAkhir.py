b4fc0200464ba99ee8bc75f1afde4136a8166479
fazamumtaz <fazamumtazramadhan@gmail.com>
2024-09-11 09:41:56 +0700

    latihan quiz dasprog

--- Kuliah/nilaiAkhir.py
+++ Kuliah/nilaiAkhir.py
@@ -6,16 +6,19 @@ nilUTSP = int(input("UTSP= "))
 # Menghitung nilai akhir 
 nilaiAkhir = (nilUAS * 0.4) + (nilUASP * 0.15) + (nilUTS * 0.3) + (nilUTSP * 0.15)
 
-if nilaiAkhir >= 80:
-    hurufMutu = 'A'
-elif nilaiAkhir >= 70:
-    hurufMutu = 'B'
-elif nilaiAkhir >= 60:
-    hurufMutu = 'C'
-elif nilaiAkhir >= 40:
-    hurufMutu = 'D'
-else : 
-    hurufMutu = 'E'
+if nilaiAkhir <= 100:
+    if nilaiAkhir >= 80:
+        hurufMutu = 'A'
+    elif nilaiAkhir >= 70:
+        hurufMutu = 'B'
+    elif nilaiAkhir >= 60:
+        hurufMutu = 'C'
+    elif nilaiAkhir >= 40:
+        hurufMutu = 'D'
+    else : 
+        hurufMutu = 'E'
+else: 
+    hurufMutu = "Nilai nguawor"
 
 # if 80 <= nilaiAkhir <= 100:
 #     hurufMutu = 'A'
